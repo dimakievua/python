917d4c1d2868992fa7d029a1676449b65ea36ee5
Dmitriy Zhernosekov <d.zhernosekov@helsi.me>
2017-12-12 00:31:21 +0200

    First commit

---
+++ polls/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+# First app
+#from django.http import HttpResponse
+#
+#def index(request):
+#    return HttpResponse("Hello, world. You're at the polls index.")
